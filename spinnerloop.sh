8fe38c15d03bd5b85e3ff5fd8f92554debf5da5f
chris4727 <123905042+chris4727@users.noreply.github.com>
2025-09-13 13:49:13 -0700

    Added spinnerloop.sh

---
+++ spinnerloop.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+spinner=( "[    ]" "[=   ]" "[==  ]" "[=== ]" "[====]" "[ ===]" "[  ==]" "[   =]" )
+#spinner=( "🕛" "🕐" "🕑" "🕒" "🕓" "🕔" "🕕" "🕖" "🕗" "🕘" "🕙" "🕚" )
+i=0
+
+while pgrep -f /usr/bin/ghb > /dev/null; do
+  printf "\e[H\e[2J"
+  eza -lah
+  echo -ne "\r\nHandbrake is Transcoding... ${spinner[$i]}"
+  ((i=(i+1)%${#spinner[@]}))
+  sleep 0.5 
+done
+
