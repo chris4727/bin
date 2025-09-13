da9e3bdb41885611d1be54a7079f57f8dfd4f6e4
Layka47 <chris47.27@proton.me>
2025-09-13 11:57:18 -0700

    Addint astro.sh

---
+++ astro.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/sh
+# ==  weather  ======================================
+# Description:
+#   Gets the weather for the current location, or accepts optional arguments
+#   for the location
+# Author: chris4727
+# Dependancies:
+# - curl
+
+check_dependencies() {
+  if ! command -v "astroterm" &> /dev/null
+  then
+    printf "${red}Error:${nc} astroterm is not installed\n"
+    exit 1
+  fi
+}
+
+run_astroterm() {
+  astroterm --color --constellations --speed 100 --fps 8 --city Seattle
+}
+
+main() {
+  check_dependencies
+  run_astroterm
+}
+
+main "$@"
+
