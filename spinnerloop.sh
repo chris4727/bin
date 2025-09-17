c83738ddfcc61102391cf337dccf24c21f666527
chris4727 <123905042+chris4727@users.noreply.github.com>
2025-09-17 11:16:08 -0700

    spinnerloop: Added new animations using braille

--- spinnerloop.sh
+++ spinnerloop.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-spinner=( "[    ]" "[=   ]" "[==  ]" "[=== ]" "[====]" "[ ===]" "[  ==]" "[   =]" )
+#spinner=( "⠋" "⠙" "⠹" "⠸" "⠼" "⠴" "⠦" "⠧" "⠇" "⠏" )
+spinner=( "⢎⡰" "⢎⡡" "⢎⡑" "⢎⠱" "⠎⡱" "⢊⡱" "⢌⡱" "⢆⡱" )
+#spinner=( "   " "   " ".  " ".. " "..." "..." "..." )
+#spinner=( "[    ]" "[=   ]" "[==  ]" "[=== ]" "[====]" "[ ===]" "[  ==]" "[   =]" )
 #spinner=( "🕛" "🕐" "🕑" "🕒" "🕓" "🕔" "🕕" "🕖" "🕗" "🕘" "🕙" "🕚" )
 i=0
 
 while pgrep -f /usr/bin/ghb > /dev/null; do
   printf "\e[H\e[2J"
   eza -lah
-  echo -ne "\r\nHandbrake is Transcoding... ${spinner[$i]}"
+  echo -ne "\r\nHandbrake is Transcoding ${spinner[$i]}"
   ((i=(i+1)%${#spinner[@]}))
-  sleep 0.5 
+  sleep 0.2 
 done
 
