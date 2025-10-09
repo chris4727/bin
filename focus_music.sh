f2e28d85a8aab49df0f7bf49fc1816f7b3cb2c44
Layka47 <chris47.27@proton.me>
2025-10-08 19:13:17 -0700

    focus_music: Added case statement to select music
    focus_music: Added case statement to select music

--- focus_music.sh
+++ focus_music.sh
@@ -8,13 +8,12 @@
 # TODO
 # - [x] Stop playing command
 # - [ ] Accept optional url as argument
-# - [ ] Select from several predefined streams
+# - [x] Select from several predefined streams
 # - [ ] Select from local playlist
 # - [ ] Play spinner while playing music. ascii cd or tape?
 
 vars() {
   url="$1"
-  stream_url="https://www.youtube.com/watch?v=UI5NKkW8acM&pp=ygUIc3RlZXp5YWY%3D" # Steezyaf Coffee Shop Radio
   red="\033[0;31m"
   yel="\033[0;33m"
   grn="\033[0;32m"
@@ -43,6 +42,26 @@ check_args() {
   fi
 }
 
+select_music() {
+  printf "${blu} ::${nc} What do you want to listen to? (${prp}1 or 2${nc})\n"
+  printf "${grn}   1.${nc} ${prp}Nujabes${nc} Instrumental mix\n"
+  printf "${grn}   2.${nc} ${prp}Steezyaf${nc} Coffee Shop Radio\n"
+  
+  read -p "Enter selection (1-2): " choice
+  
+  case $choice in 
+    1)
+      stream_url="https://www.youtube.com/watch?v=z3aS5AwhJSU&list=RDz3aS5AwhJSU&start_radio=1" # Nujabes playlist
+      ;;
+    2)
+      stream_url="https://www.youtube.com/watch?v=UI5NKkW8acM&pp=ygUIc3RlZXp5YWY%3D" # Steezyaf Coffee Shop Radio
+      ;;
+    *)
+      printf "${yel}Invalid choice:${nc} Enter a number bewtween 1 and 2\n"
+      ;;
+  esac 
+}
+
 play_music() {
   printf "${blu} ::${nc} ${grn}Playing...${nc}\n"
   printf "${blu} ::${nc} Press ${prp}Ctrl+c${nc} to stop\n"
@@ -54,6 +73,7 @@ main() {
   check_dependencies
   #echo_vars
   check_args "$@"
+  select_music
   play_music
 }
 
